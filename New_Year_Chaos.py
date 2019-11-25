df0071967a9e4977dc308a7f28410c38a686a9d9
gourav287 <57107567+gourav287@users.noreply.github.com>
2019-11-25 08:08:04 +0530

    Add files via upload

---
+++ New_Year_Chaos.py
@@ -0,0 +1,33 @@
+"""
+Question Link:
+https://www.hackerrank.com/challenges/new-year-chaos/problem
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    s=0
+    for i in range(len(q)):
+        if (q[i]-i>3):
+            print("Too chaotic")
+            return
+        else:
+            for j in range(max(0,q[i]-2),i):
+                if q[j]>q[i]:
+                    s+=1
+    print(s)
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
