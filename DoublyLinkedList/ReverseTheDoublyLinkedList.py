086affef3797ad13179a5483a271ccec072c35b6
Gourav <gourav.saxena.9822@gmail.com>
2020-08-10 19:47:08 +0530

    Add comments to the 'ReverseTheDoublyLinkedList.py' file

--- DoublyLinkedList/ReverseTheDoublyLinkedList.py
+++ DoublyLinkedList/ReverseTheDoublyLinkedList.py
@@ -3,7 +3,7 @@ This piece of code contains implementation of doubly linked list and
 reversal of the list
 """
 
-# The node structure
+# The node structure through a class
 class ListNode:
 
     def __init__(self, data):
@@ -71,21 +71,25 @@ class LinkedList:
         print("\n")
 
 """
-Implementation of node deletion
+Implementation of Reversal of Doubly Linked List
 """
 def ReverseNode(start):
 
     # Check if list exists and has more than 1 elements else do nothing
     if start and start.next:
 
+        # Working with three pointers, previous, current and next
         prev_node, cur_node, nex_node = None, start, start.next
 
+        # Traversing through the list
         while cur_node:
-
+            
+            # Interchange current nodes prev and next pointers in 2 steps
             cur_node.prev = cur_node.next
 
             cur_node.next = prev_node
 
+            # Move all the 3 pointers one step ahead to work on next node
             prev_node = cur_node
             
             cur_node = nex_node
@@ -94,8 +98,10 @@ def ReverseNode(start):
 
                 nex_node = nex_node.next
 
+        # Assign head pointer to the last node which is now the first
         start = prev_node
 
+    # Return head pointer
     return start
 
 
@@ -123,5 +129,5 @@ if __name__ == '__main__':
     start.head = ReverseNode(start.head)
 
     # Printing the updated list
-    print("Updated list:")
+    print("Reversed list:")
     start.printlist()
